69d3cfcbb3a42b0df1f045a06e455bb76382c55f
Sebastian Berns <s.berns@qmul.ac.uk>
2022-02-03 15:36:14 +0000

    Removed download_google_drive()

--- helpers.py
+++ helpers.py
@@ -26,32 +26,3 @@ def check_download_url(local_dir, url):
         with urllib.request.urlopen(url) as response, open(file_path, 'wb') as f:
             shutil.copyfileobj(response, f)
     return file_path
-
-
-"""
-Download a file from google drive
-
-    file_id: id of the google drive file
-    out_path (str): output folder path
-"""
-def download_google_drive(file_id, out_path):
-    def get_confirm_token(response):
-        for key, value in response.cookies.items():
-            if key.startswith('download_warning'):
-                return value
-        return None
-
-    URL = "https://drive.google.com/uc?export=download"
-    session = requests.Session()
-    response = session.get(URL, params={'id': file_id}, stream=True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = {'id': file_id, 'confirm': token}
-        response = session.get(URL, params=params, stream=True)
-
-    CHUNK_SIZE = 32768
-    with open(out_path, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk:
-                f.write(chunk)
