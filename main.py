12404eda9c6c53f2b4ffb990ad523cf9f5894e21
Sebastian Berns <s.berns@qmul.ac.uk>
2022-02-03 15:34:19 +0000

    Log level explanation

--- main.py
+++ main.py
@@ -8,13 +8,21 @@ from .inception_torchscript import InceptionV3W
 from .resize import Resizer
 
 
+"""
+Standard Python logging levels
+https://docs.python.org/3/library/logging.html#levels
+
+Higher levels (lower is value) include all lower levels.
+E.g. 'critical' only shows critical messages, whereas 'warning' will show
+warnings, errors and critical messages.
+"""
 loglevels = {
-    'all': 0,
+    'all': 0,  # Log all messages
     'debug': 10,
     'info': 20,
     'warning': 30,
-    'error': 40,
-    'critical': 50
+    'error': 40,  # Log both errors and critical messages
+    'critical': 50  # Only log critical messages
 }
 
 
@@ -26,8 +34,9 @@ class CleanFeatures:
         device (str or device): Device to run model on (e.g. 'cuda' or 'cpu')
     """
     def __init__(self, model_path, device=None, log='warning'):
-        if not device:
-            self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        # If device == None, set to cuda if available, otherwise set to cpu
+        device = device or 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.device = torch.device(device)
 
         logging.basicConfig(format='%(message)s', level=loglevels[log])
 
@@ -40,7 +49,8 @@ class CleanFeatures:
         logging.info('CleanFeatures ready.')
 
     """
-    Compute features of images in the embedding space of an embedding model
+    Compute features of images in the embedding space of a deep computer
+    vision model
 
         images (Pytorch tensor [B, C, W, H]): Batch of images with values in range (0, 255)
 
