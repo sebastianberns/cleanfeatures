c9922846d224ed42704c61ccd3c0de7ad627cae3
Sebastian Berns <s.berns@qmul.ac.uk>
2023-01-04 17:39:16 +0000

    Support datasets without targets

--- dataset.py
+++ dataset.py
@@ -6,7 +6,7 @@ from torch import Tensor
 from torch.utils.data import Dataset
 
 
-class CleanFeaturesDataset(Dataset[Tuple[Tensor, Tensor]]):
+class CleanFeaturesDataset(Dataset[Tuple[Tensor, Optional[Tensor]]]):
     r"""Clean features dataset wrapper
 
     Each sample will be retrieved by indexing the features tensor along the
@@ -30,12 +30,15 @@ class CleanFeaturesDataset(Dataset[Tuple[Tensor, Tensor]]):
         self.data = self.features  # Alias
         self.num_features = self.features.size(1)
 
-        assert self.features.size(0) == len(self.targets), (f"Size mismatch "
-            f"between features ({self.features.size(0):,} samples) and "
-            f"targets ({len(self.targets):,} labels)")
+        if self.targets:
+            assert self.features.size(0) == len(self.targets), (f"Size mismatch "
+                f"between features ({self.features.size(0):,} samples) and "
+                f"targets ({len(self.targets):,} labels)")
 
-    def __getitem__(self, index) -> Tuple[Tensor, Tensor]:
-        return self.features[index], self.targets[index]
+    def __getitem__(self, index) -> Tuple[Tensor, Optional[Tensor]]:
+        if self.targets:
+            return self.features[index], self.targets[index]
+        return self.features[index], None
 
     def __len__(self) -> int:
         return self.features.size(0)
